0c1754890abd1c74c0c9918f5307cbfcf588323f
Ananthu A <ananthuaa1999@gmail.com>
2021-09-14 16:53:16 +0530

    Use StringUtrils and added dependency to lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,18 +2,22 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
-	public void greet() {
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}	
+	public void greet(){
 		List<String> greetings = new ArrayList<>();
-		greetings.add("Hello");
+		greetings.add("HELLO");
 		
 		for (String greeting : greetings) {
-			System.out.println("Greeting: " + greeting);
+			System.out.println("Greeting:"+ greeting);
 		}
-	}
-	
+	}	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -21,7 +25,9 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
-	app.greet();
+	    Application app = new Application();
+		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println("Word Count: " + count);
     }
 }
